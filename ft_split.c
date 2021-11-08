b9f3ef79e0705e4c63a54fd4a3abf91e9f68d50b
Taha Chtaibi <tchtaibi@e1r2p8.1337.ma>
2021-11-08 17:34:22 +0100

    pussh

--- ft_split.c
+++ ft_split.c
@@ -11,9 +11,65 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+size_t ft_strlenn(char const *str, char sep)
+{
+    size_t i;
 
+    i = 0;
+    while(str[i] != sep)
+        i++;
+    return (i);
+}
 char **ft_split(char const *s, char c)
 {
-	int i;
-	
+	size_t i;
+    size_t ii;
+    size_t j;
+    size_t count;
+	char **new;
+
+    count = 0;
+    i = 0;
+    ii = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            count++;
+        i++;
+    }
+    i = 0;
+    *new = malloc(count * (ft_strlen(s) + 1));
+    while (count > i)
+    {
+        new[i] = malloc (ft_strlenn())
+    }
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+        {
+            new[j][ii] = '\0';
+            j++;
+            ii = 0;
+        }
+        new[j][ii] = s[ii];
+        i++;
+        ii++;
+    }
+    new[j][ii] = '\0';
+    return (new);
+}
+
+int main()
+{
+    char s[] = "abcd+efgh+ijkl";
+    char **new = ft_split(s,'+');
+    size_t i;
+
+    i = 0;
+    while (new[i])
+    {
+        prsize_tf("%s",new[i]);
+        i++;
+    }
 }
